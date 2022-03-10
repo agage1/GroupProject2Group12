8fda49faedb2ab39e47cd36688e7a2512bccca4f
Scott1536 <smarti96@vols.utk.edu>
2022-03-09 20:19:41 -0500

    Prototype framework
    Prototype framework

--- Scott_Code/server.R
+++ Scott_Code/server.R
@@ -1,1 +1,14 @@
+library(shiny)
 
+ui <- fluidPage(
+  dateRangeInput("dates", label = h3("Date range")),
+  
+  hr(),
+  fluidRow(column(4, verbatimTextOutput("value")))
+)
+
+server <- function(input, output, session) {
+  output$value <- renderPrint({ input$dates })
+}
+
+shinyApp(ui, server)
