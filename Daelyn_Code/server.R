7e95135d71a73649d3cdd25b78e5f77de4c69048
Daelyn Greene <dgreen41@vols.utk.edu>
2022-03-09 19:43:21 -0500

    added a display of what stock is selected
    the stuff that is commented out is still being worked on. i eventually want the user to be able to select multiple stocks and get to the yahoo finance page for all of them, but thats not being too forthcoming on how to do it

--- Daelyn_Code/server.R
+++ Daelyn_Code/server.R
@@ -2,14 +2,22 @@ server <- function(input, output, session) {
   
   
   
+  output$text <- renderUI({HTML(paste(input$select,collapse = "<br/>") )})
+  
+  
+  
   output$dropdown <- renderUI(
   pickerInput(
     inputId = "select",
+    # multiple = T,
     label = h3("Choose Your Stock"), 
     choices = sort(SYMBOLS$SYMBOLNAMECOMBO),
     selected = SYMBOLS$SYMBOLNAMECOMBO[which(SYMBOLS$Symbol == "MSFT")],
     options = list(
-      `live-search` = TRUE)
+      `live-search` = TRUE
+      # ,
+      # `selected-text-format` = "count"
+      )
   ))
   
   
@@ -24,6 +32,5 @@ server <- function(input, output, session) {
     a(href=barelink,"See your stock on Yahoo Finance.",target="_blank")
   })
   
-  
-  
 }
+
--- Daelyn_Code/ui.R
+++ Daelyn_Code/ui.R
@@ -1,5 +1,7 @@
 ui <- fluidPage(
   uiOutput("dropdown"),
   br(),
+  uiOutput("text"),
+  br(),
   fluidRow(column(3, uiOutput("link")))
 )
