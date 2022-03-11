d84788ee19b7ba38610f0bd4a7810215c995c4eb
David Carlton <99289205+CarltonDavid@users.noreply.github.com>
2022-03-11 16:34:42 -0500

    Added Dividend Summation

--- David_Code/global.R
+++ David_Code/global.R
@@ -1,12 +1,18 @@
-library(shiny)
-library(shinydashboard)
-library(quantmod)
-library(shinyWidgets)
 library(dplyr)
 library(plotly)
 library(fpp3)
+library(xts)
+library(lubridate)
+library(quantmod)
 
 SYMBOLS <- stockSymbols()
+
 YAHOOURLSTART <- "https://finance.yahoo.com/quote/"
+
 SYMBOLS <- SYMBOLS %>% 
   mutate(SYMBOLNAMECOMBO = paste(Symbol,"-",Name))
+
+clean_names <- function(stocks) {
+  split_names <- strsplit(names(stocks), split = ".", fixed = TRUE)
+  vapply(split_names, function(x) x[2], character(1))
+}
